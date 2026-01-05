8ef960eb191bf0e389f5aa211b89fe71cb605c0d
Vend-Jinwoo <effendinursalim55@gmail.com>
2026-01-05 14:23:18 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+const button = document.querySelector(".btn");
+
+button.addEventListener("click", function () {
+  window.open("https://wa.me/6285828946500?text=Halo%20Vend,%20gua%20lihat%20website%20lu. Erine bilang lu pacar dia yaa",
+  "_blank")
+});
